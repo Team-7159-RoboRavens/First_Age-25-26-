1fe93c6bf8e6bfd6a0d9fe4d30bfd360d7045b2f
TheMarsianator <Liammarzell@gmail.com>
2025-10-18 10:25:58 -0700

    Pedro Pathing Mock Up
    This is just some code generated by the PedroPathing browser site.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous/bigPedro.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.Autonmous;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Medium Pedro")
+public class bigPedro {
+
+
+        public bigPedro() {
+//            PathBuilder builder = new PathBuilder();
+//
+//    builder
+//      .addPath(
+//        // Line 1
+//        new BezierLine(
+//          new Point(55.000, 9.000, Point.CARTESIAN),
+//          new Point(130.000, 9.000, Point.CARTESIAN)
+//        )
+//      )
+            //Pick up Balls
+//      .setTangentHeadingInterpolation()
+//      .addPath(
+//        // Line 2
+//        new BezierLine(
+//          new Point(130.000, 9.000, Point.CARTESIAN),
+//          new Point(60.000, 9.000, Point.CARTESIAN)
+//        )
+//      )
+            //Shoot Balls
+//      .setTangentHeadingInterpolation()
+//      .addPath(
+//        // Line 3
+//        new BezierLine(
+//          new Point(60.000, 9.000, Point.CARTESIAN),
+//          new Point(130.000, 9.000, Point.CARTESIAN)
+//        )
+//      )
+            //Pick Up balls
+//      .setTangentHeadingInterpolation()
+//      .addPath(
+//        // Line 4
+//        new BezierLine(
+//          new Point(130.000, 9.000, Point.CARTESIAN),
+//          new Point(60.000, 9.000, Point.CARTESIAN)
+//        )
+//      )
+//      .setTangentHeadingInterpolation();
+            //Shoot Balls
+    }
+
+
+}

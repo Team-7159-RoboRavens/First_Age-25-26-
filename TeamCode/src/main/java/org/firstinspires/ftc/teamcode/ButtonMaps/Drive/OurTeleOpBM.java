516eea1f80bf0d9c67809aa3dfe1f50d36a607ca
TheMarsianator <Liammarzell@gmail.com>
2025-10-19 10:04:42 -0700

    Adding back FOD

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/OurTeleOpBM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/OurTeleOpBM.java
@@ -8,10 +8,19 @@ import org.firstinspires.ftc.teamcode.ButtonMaps.MotorPowers;
 import org.firstinspires.ftc.teamcode.ComplexRobots.FirstAgeTempbot;
 
 public class OurTeleOpBM extends AbstractButtonMap {
-
+    MotorPowers mp = new MotorPowers(0);
     @Override
     public void loop(FirstAgeTempbot robot, OpMode opMode) {
-        MotorPowers mp = HolonomicDrive.fieldOrientedDrive(opMode.gamepad1, 1.0);
+        if (Math.abs(opMode.gamepad1.left_stick_y) > .2) {
+            mp = HolonomicDrive.fieldOrientedDrive(opMode.gamepad1, .8, robot.lazyImu.get());
+        }
+        else if (Math.abs(opMode.gamepad1.left_stick_x) > .2) {
+            mp = HolonomicDrive.fieldOrientedDrive(opMode.gamepad1, .8, robot.lazyImu.get());
+        }
+
+        if (opMode.gamepad1.options) {
+            robot.lazyImu.get().resetYaw();
+        }
 
         opMode.telemetry.update();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/HolonomicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/HolonomicDrive.java
@@ -9,20 +9,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 public class HolonomicDrive {
     //thanks game manual 0!
     //https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
-    public static MotorPowers fieldOrientedDrive(Gamepad gamepad, double maxMotorPower){
-
+    public static MotorPowers fieldOrientedDrive(Gamepad gamepad, double maxMotorPower, IMU imu){
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
         //Provide a deadzone of +-0.1
-        double x = gamepad.left_stick_x > 0.1 || gamepad.left_stick_x < -0.1 ? gamepad.left_stick_x : 0;
-        double y = gamepad.left_stick_y > 0.1 || gamepad.left_stick_y < -0.1 ? -gamepad.left_stick_y : 0;
+        double x = gamepad.left_stick_x;
+        double y = -gamepad.left_stick_y;
         double rotate = gamepad.right_stick_x > 0.1 || gamepad.right_stick_x < -0.1 ? gamepad.right_stick_x : 0;
-        double rotX = x * Math.cos(0) - y * Math.sin(0);
-        double rotY = x * Math.sin(0) + y * Math.cos(0);
+        double rotX = (x * Math.cos(-botHeading) - y * Math.sin(-botHeading)) * 1.1;
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rotate), 1);
         double frontLeftPower = maxMotorPower*((rotY + rotX + rotate) / denominator);
         double backLeftPower = maxMotorPower*((rotY - rotX + rotate) / denominator);
         double frontRightPower = maxMotorPower*((rotY - rotX - rotate) / denominator);
         double backRightPower = maxMotorPower*((rotY + rotX - rotate) / denominator);
-//        frontLeftPower *= .5; backLeftPower *= .5; frontRightPower *= .5; backRightPower *= .5;
+        frontLeftPower *= .5; backLeftPower *= .5; frontRightPower *= .5; backRightPower *= .5;
         return new MotorPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
     }
 

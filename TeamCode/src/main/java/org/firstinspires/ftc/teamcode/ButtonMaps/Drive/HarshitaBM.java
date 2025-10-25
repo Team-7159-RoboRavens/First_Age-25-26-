3c1b2a5fb1c84f55a9084829fdd47eb8c00d57a9
ryzencatz <ryzencatz@gmail.com>
2025-10-25 10:26:01 -0700

    made some changes to HarshitaBM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/HarshitaBM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/HarshitaBM.java
@@ -12,93 +12,87 @@ import org.firstinspires.ftc.teamcode.ComplexRobots.FirstAgeTempbot;
 public class HarshitaBM extends AbstractButtonMap {
 
     private MotorPowers mp; // = new MotorPowers(0);
-    private boolean servoState=false;
 
     @Override
     public void loop(FirstAgeTempbot robot, OpMode opMode) {
         mp = new MotorPowers(0);
-        double multiplier = 1;
+        double multiplier = 1; // mainly for the dpad
 
-        //Increases speed when right bumper is pressed by 0.1
+        //Increases speed (multiplier) when right bumper is pressed by 0.1
         if (opMode.gamepad1.right_bumper) {
             multiplier += 0.1;
-            //
         }
-        //Decreases speed when left bumper is pressed by 0.1
+        //Decreases speed (multiplier) when left bumper is pressed by 0.1
         if (opMode.gamepad1.left_bumper) {
             multiplier -= 0.1;
         }
 
-        // Hard stop when x is pressed
-        if (opMode.gamepad1.x) {
+        // Hard stop when b is pressed
+        if (opMode.gamepad1.b) {
             mp = new MotorPowers(0);
         }
-        // open / closes  servo when a is pressed
-        if (opMode.gamepad1.a) {
-            servoState = !servoState;
-        }
+
 
         // When left trigger is pressed, turn left while staying in place
-        if (opMode.gamepad1.left_trigger>0.3){
-            mp = new MotorPowers(1 * multiplier * opMode.gamepad1.left_trigger,
-                    -1 * multiplier * opMode.gamepad1.left_trigger,
-                    1 * multiplier * opMode.gamepad1.left_trigger,
-                    -1 * multiplier* opMode.gamepad1.left_trigger);
+        if (opMode.gamepad1.left_trigger>0.1){
+            mp = new MotorPowers(1 * opMode.gamepad1.left_trigger,
+                    -1 * opMode.gamepad1.left_trigger,
+                    1 * opMode.gamepad1.left_trigger,
+                    -1 * opMode.gamepad1.left_trigger);
         }
         // When right trigger is pressed, turn right while staying in place
-        if (opMode.gamepad1.right_trigger>0.3){
+        if (opMode.gamepad1.right_trigger>0.1){
             mp = new MotorPowers(-1 * multiplier * opMode.gamepad1.right_trigger,
-                    1 * multiplier * opMode.gamepad1.right_trigger,
-                    -1 * multiplier * opMode.gamepad1.right_trigger,
-                    1 * multiplier* opMode.gamepad1.right_trigger);
+                    1 * opMode.gamepad1.right_trigger,
+                    -1 * opMode.gamepad1.right_trigger,
+                    1 * opMode.gamepad1.right_trigger);
         }
 
 
         if (opMode.gamepad1.dpad_down) {
-            // what is dpad?
             //When dpad down-left is pressed, moves diagonally down-left
             if (opMode.gamepad1.dpad_left) {
-                mp = new MotorPowers(-1 * multiplier,
+                mp = new MotorPowers(1 * multiplier,
                         0 * multiplier,
                         0 * multiplier,
-                        -1 * multiplier);
+                        1 * multiplier);
             }
             //When dpad down-right is pressed, move diagonally down-right
             else if (opMode.gamepad1.dpad_right){
                 mp = new MotorPowers(0 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier,
+                        1 * multiplier,
+                        1 * multiplier,
                         0 * multiplier);
             }
             //When dpad down is pressed, move straight down
             else {
-                mp = new MotorPowers(-1 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier);
+                mp = new MotorPowers(1 * multiplier,
+                        1 * multiplier,
+                        1 * multiplier,
+                        1 * multiplier);
             }
         } else if (opMode.gamepad1.dpad_up) {
             //When dpad up-left is pressed, move diagonally up-left
             if (opMode.gamepad1.dpad_left) {
                 mp = new MotorPowers(0 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier,
+                        -1 * multiplier,
+                        -1 * multiplier,
                         0 * multiplier);
             }
             //When dpad up-right is pressed, move diagonally up-right
            else if (opMode.gamepad1.dpad_right) {
-                mp = new MotorPowers(1 * multiplier,
+                mp = new MotorPowers(-1 * multiplier,
                         0 * multiplier,
                         0 * multiplier,
-                        1 * multiplier);
+                        -1 * multiplier);
             //When dpad up is pressed, move straight up
             } else {
-                mp = new MotorPowers(1 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier);
+                mp = new MotorPowers(-1 * multiplier,
+                        -1 * multiplier,
+                        -1 * multiplier,
+                        -1 * multiplier);
             }
-           //When dpad right is pressed, move straight right
+           //When dpad right is pressed, move strafe right
         } else if (opMode.gamepad1.dpad_right){
             mp = new MotorPowers(1 * multiplier,
                     -1 * multiplier,
@@ -116,47 +110,32 @@ public class HarshitaBM extends AbstractButtonMap {
         } */
 
 
-        //Allows Joystick to control where the robot goes
-        if (Math.abs(opMode.gamepad1.right_stick_x) > 0.4 || Math.abs(opMode.gamepad1.right_stick_y) > 0.4) {
+        // Allows Joystick to control where the robot goes
+        /*if (Math.abs(opMode.gamepad1.right_stick_x) > 0.4 || Math.abs(opMode.gamepad1.right_stick_y) > 0.4) {
             // pythagorean theorem to calculate magnitude of joystick vector
             double speed = Math.sqrt(opMode.gamepad1.right_stick_y * opMode.gamepad1.right_stick_y + opMode.gamepad1.right_stick_x * opMode.gamepad1.right_stick_x);
 
-            double speedX = speed - 2 * opMode.gamepad1.right_stick_x * opMode.gamepad1.right_stick_x;
-            double speedY = speed - 2 * opMode.gamepad1.right_stick_y * opMode.gamepad1.right_stick_y;
-
-            if (opMode.gamepad1.right_stick_x >= 0 && opMode.gamepad1.right_stick_y <= 0) {
+            if (opMode.gamepad1.right_stick_x >= 0.1 && opMode.gamepad1.right_stick_y <= 0.1) {
                 // first quadrant
-                mp = new MotorPowers(speed,
-                        speedX,
-                        speedX,
-                        speed);
-            } else if (opMode.gamepad1.right_stick_x >= 0 && opMode.gamepad1.right_stick_y >= 0) {
+
+            } else if (opMode.gamepad1.right_stick_x >= 0.1 && opMode.gamepad1.right_stick_y >= 0.1) {
                 // fourth quadrant
-                mp = new MotorPowers(speedY,
-                        -speed,
-                        -speed,
-                        speedY);
-            } else if (opMode.gamepad1.right_stick_x <= 0 && opMode.gamepad1.right_stick_y  >= 0) {
+
+            } else if (opMode.gamepad1.right_stick_x <= 0.1 && opMode.gamepad1.right_stick_y  >= 0.1) {
                 // third quadrant
-                mp = new MotorPowers(-speed,
-                        speedY,
-                        speedY,
-                        -speed);
+
             } else {
                 // second quadrant
-                mp = new MotorPowers(speedX,
-                        speed,
-                        speed,
-                        speedX);
+
             }
             //mp = HolonomicDrive.JoystickHoloDrive(opMode.gamepad1);
-        }
+        } */
+        // holonomic drive with left joystick
         if (Math.abs(opMode.gamepad1.left_stick_x) > 0.4 || Math.abs(opMode.gamepad1.left_stick_y) > 0.4) {
-
             mp = HolonomicDrive.JoystickHoloDrive(opMode.gamepad1, opMode);
+        }
 
-       }
-            mp = new MotorPowers(mp.leftFront, mp.rightFront, mp.leftBack, mp.rightBack);
+        mp = new MotorPowers(mp.leftFront, mp.rightFront, mp.leftBack, mp.rightBack);
 
         opMode.telemetry.update();
         robot.setMotorPowers(mp);

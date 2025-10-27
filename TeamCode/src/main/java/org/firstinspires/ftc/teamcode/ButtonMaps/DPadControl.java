feb6ba66675e48a90a4fefaa8d9f00ebac1e0ed3
ryzencatz <ryzencatz@gmail.com>
2025-10-26 22:28:10 -0700

    Update DPadControl
    now it uses the HolonomicDrive.robotOrientedDrive method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/DPadControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/DPadControl.java
@@ -4,65 +4,28 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class DPadControl {
-
     public static MotorPowers dpadStrafe(OpMode opMode, double multiplier) {
         MotorPowers mp = new MotorPowers(0);
         if (opMode.gamepad1.dpad_down) {
             //When dpad down-left is pressed, moves diagonally down-left
-            if (opMode.gamepad1.dpad_left) {
-                mp = new MotorPowers(-1 * multiplier,
-                        0 * multiplier,
-                        0 * multiplier,
-                        -1 * multiplier);
-            }
+            if (opMode.gamepad1.dpad_left) mp = HolonomicDrive.robotOrientedDrive(-1, -1, 0, multiplier);
             //When dpad down-right is pressed, move diagonally down-right
-            else if (opMode.gamepad1.dpad_right) {
-                mp = new MotorPowers(0 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier,
-                        0 * multiplier);
-            }
+            else if (opMode.gamepad1.dpad_right) mp = HolonomicDrive.robotOrientedDrive(1, -1, 0, multiplier);
             //When dpad down is pressed, move straight down
-            else {
-                mp = new MotorPowers(-1 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier,
-                        -1 * multiplier);
-            }
+            else mp = HolonomicDrive.robotOrientedDrive(0, -1, 0, multiplier);
         } else if (opMode.gamepad1.dpad_up) {
             //When dpad up-left is pressed, move diagonally up-left
-            if (opMode.gamepad1.dpad_left) {
-                mp = new MotorPowers(0 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier,
-                        0 * multiplier);
-            }
+            if (opMode.gamepad1.dpad_left) mp = HolonomicDrive.robotOrientedDrive(-1, 1, 0, multiplier);
             //When dpad up-right is pressed, move diagonally up-right
-            else if (opMode.gamepad1.dpad_right) {
-                mp = new MotorPowers(1 * multiplier,
-                        0 * multiplier,
-                        0 * multiplier,
-                        1 * multiplier);
-                //When dpad up is pressed, move straight up
-            } else {
-                mp = new MotorPowers(1 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier,
-                        1 * multiplier);
-            }
-            //When dpad right is pressed, move straight right
-        } else if (opMode.gamepad1.dpad_right) {
-            mp = new MotorPowers(1 * multiplier,
-                    -1 * multiplier,
-                    -1 * multiplier,
-                    1 * multiplier);
-            //When dpad left is pressed, move straight left
-        } else if (opMode.gamepad1.dpad_left) {
-            mp = new MotorPowers(-1 * multiplier,
-                    1 * multiplier,
-                    1 * multiplier,
-                    -1 * multiplier);
+            else if (opMode.gamepad1.dpad_right) mp = HolonomicDrive.robotOrientedDrive(1, 1, 0, multiplier);
+            //When dpad up is pressed, move straight up
+            else mp = HolonomicDrive.robotOrientedDrive(0, 1, 0, multiplier);
         }
+        //When dpad right is pressed, move straight right
+        else if (opMode.gamepad1.dpad_right) mp = HolonomicDrive.robotOrientedDrive(1, 0, 0, multiplier);
+        //When dpad left is pressed, move straight left
+        else if (opMode.gamepad1.dpad_left) mp = HolonomicDrive.robotOrientedDrive(-1, 0, 0, multiplier);
+
         return mp;
     }
 

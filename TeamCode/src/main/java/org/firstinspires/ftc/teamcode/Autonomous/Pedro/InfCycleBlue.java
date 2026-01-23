8df49b833082a9a4c3262aa31388b79edaed4548
MoonCaller27 <kadenrwu@gmail.com>
2026-01-23 04:15:17 +0000

    Cycle autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Pedro/InfCycleBlue.java
@@ -0,0 +1,159 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Pedro;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous(name = "InfCycleBlue")
+public class InfCycleBlue extends OpMode {
+
+    private Follower follower;
+    private Timer stateTimer;
+    private Timer autoTimer;
+    private static final double SHOOT_TIME = 2.0;
+    private static final double INTAKE_TIME = 1.5;
+    private static final double AUTO_END_TIME = 27.0;
+
+    enum AutoState {
+        START_TO_SHOOT,
+        SHOOT,
+        SHOOT_TO_PICKLOAD,
+        PICKLOAD,
+        PICKLOAD_TO_SHOOT,
+        PARK,
+        DONE
+    }
+
+    private AutoState state;
+    Pose startPose   = new Pose(56.5, 8, Math.toRadians(90));
+    Pose shootPose   = new Pose(61, 12, Math.toRadians(112.28));
+    Pose pickLoadPose = new Pose(10.436, 7.5079, Math.toRadians(180));
+    Pose parkPose = new Pose(48.0849, 22.407, Math.toRadians(180));
+
+    PathChain startToShoot;
+    PathChain shootToPickLoad;
+    PathChain pickLoadToShoot;
+    PathChain shootToPark;
+
+    void buildPaths() {
+        startToShoot = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, shootPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), shootPose.getHeading())
+                .build();
+
+        shootToPickLoad = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, pickLoadPose))
+                .setLinearHeadingInterpolation(shootPose.getHeading(), pickLoadPose.getHeading())
+                .build();
+
+        pickLoadToShoot = follower.pathBuilder()
+                .addPath(new BezierLine(pickLoadPose, shootPose))
+                .setLinearHeadingInterpolation(pickLoadPose.getHeading(), shootPose.getHeading())
+                .build();
+
+        shootToPark = follower.pathBuilder()
+                .addPath(new BezierLine(shootPose, parkPose))
+                .setLinearHeadingInterpolation(shootPose.getHeading(), parkPose.getHeading())
+                .build();
+    }
+
+    void setState(AutoState newState) {
+        state = newState;
+        stateTimer.resetTimer();
+
+        switch (newState) {
+            case START_TO_SHOOT:
+                follower.followPath(startToShoot, true);
+                break;
+            case SHOOT:
+                // shooting code
+                break;
+            case SHOOT_TO_PICKLOAD:
+                follower.followPath(shootToPickLoad, true);
+                break;
+            case PICKLOAD:
+                // intake code
+                break;
+            case PICKLOAD_TO_SHOOT:
+                follower.followPath(pickLoadToShoot, true);
+                break;
+            case PARK:
+                follower.followPath(shootToPark, true);
+                break;
+            case DONE:
+                follower.breakFollowing();
+                break;
+        }
+    }
+
+    void updateStateMachine() {
+        if (autoTimer.getElapsedTimeSeconds() >= AUTO_END_TIME) {
+            if (state != AutoState.PARK && state != AutoState.DONE) {
+                setState(AutoState.PARK);
+                return;
+            }
+        }
+
+        switch (state) {
+            case START_TO_SHOOT:
+                if (!follower.isBusy())
+                    setState(AutoState.SHOOT);
+                break;
+
+            case SHOOT:
+                if (stateTimer.getElapsedTimeSeconds() >= SHOOT_TIME)
+                    setState(AutoState.SHOOT_TO_PICKLOAD);
+                break;
+
+            case SHOOT_TO_PICKLOAD:
+                if (!follower.isBusy())
+                    setState(AutoState.PICKLOAD);
+                break;
+
+            case PICKLOAD:
+                if (stateTimer.getElapsedTimeSeconds() >= INTAKE_TIME)
+                    setState(AutoState.PICKLOAD_TO_SHOOT);
+                break;
+
+            case PICKLOAD_TO_SHOOT:
+                if (!follower.isBusy())
+                    setState(AutoState.SHOOT);
+                break;
+
+            case PARK:
+                if (!follower.isBusy())
+                    setState(AutoState.DONE);
+                break;
+
+            case DONE:
+                break;
+        }
+    }
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        stateTimer = new Timer();
+        autoTimer = new Timer();
+
+        buildPaths();
+        follower.setPose(startPose);
+
+        stateTimer.resetTimer();
+        autoTimer.resetTimer();
+
+        setState(AutoState.START_TO_SHOOT);
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        updateStateMachine();
+    }
+}

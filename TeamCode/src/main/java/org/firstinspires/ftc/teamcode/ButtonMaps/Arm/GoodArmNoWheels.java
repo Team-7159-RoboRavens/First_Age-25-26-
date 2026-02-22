9c9edd64affe6b034bb67b6d75a8948b5c318e38
TheMarsianator <Liammarzell@gmail.com>
2026-02-22 15:54:41 -0800

    Quick Fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Arm/GoodArmNoWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Arm/GoodArmNoWheels.java
@@ -35,9 +35,9 @@ public class GoodArmNoWheels extends NoWheelsAbstractButtonMap {
             targetVel = velocityShot(280);
             opMode.telemetry.addData("Target Velocity", targetVel);
 
-            robot.ShootMotor.setVelocity(targetVel);
+            robot.ShootMotor.setPower((targetVel - robot.ShootMotor.getVelocity()) / 120);
 
-            robot.ShootMotor2.setVelocity(targetVel);
+            robot.ShootMotor2.setPower((targetVel - robot.ShootMotor2.getVelocity()) / 120);
         }
         else {
             robot.ShootMotor.setVelocity(0);
